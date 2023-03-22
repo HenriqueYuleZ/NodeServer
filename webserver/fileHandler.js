d8732d9a929fd55b18dd1ee337ca54bd2825ccd0
Henrique Yule <yzhenrique@gmail.com>
2023-03-21 22:28:53 -0400

    webserver com gerenciamento de arquivos

--- webserver/fileHandler.js
+++ webserver/fileHandler.js
@@ -3,9 +3,9 @@ var fs = require('fs');
 module.exports = function(filename, successFn, errorFn) {
   fs.readFile(filename, function(err, data) {
       if(err) {
-           errorFn(err);
+          errorFn(err);
       } else {
-           successFn(data);
+          successFn(data);
       }
   });
  };
